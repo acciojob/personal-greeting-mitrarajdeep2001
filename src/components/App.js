4911497f9caabc429273fdec54c0008379f6195c
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-15 10:10:16 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState('')
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" onChange={(e) => {
+          setInput(e.target.value)
+        }}/>
+        <p>{input}</p>
     </div>
   )
 }
